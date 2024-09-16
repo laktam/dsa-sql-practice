38edaf6fc79723220b691241ce67d65f5c587d8f
laktam <95582405+laktam@users.noreply.github.com>
2024-09-16 18:47:54 +0100

    Time: 662 ms (5.45%) | Memory: 45.6 MB (5.41%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++){
+            if(t.contains(Character.toString(s.charAt(i)))){
+                t = t.replaceFirst(Character.toString(s.charAt(i)), "");
+            }else{
+                return false;
+            }
+        }
+        if(t.length() == 0){
+            return true;
+        }
+        
+        return true;
+    }
+}
