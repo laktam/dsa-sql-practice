3bd7dcdc89c31ea8e62a10afe3b166154da3b7a8
laktam <95582405+laktam@users.noreply.github.com>
2024-10-29 22:12:04 +0100

    Time: 31 ms (56.94%) | Memory: 51.4 MB (64.41%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,34 +3,25 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> triplets = new ArrayList<>();
         for(int i = 0; i < nums.length - 2; i++){
-            if(i > 0 && nums[i] == nums[i - 1]){
-                continue; // avoid duplicate
-            }
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            
             int r = nums.length - 1;
             int l = i + 1;
             while(l < r){
-                int sum = nums[i] + nums[l] + nums[r];
+                int sum = nums[i] + nums[r] + nums[l];
                 if(sum == 0){
-                    triplets.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    while(l < r && nums[r] == nums[r - 1]){
-                        r--;
-                    }
-                    while(l < r && nums[l] == nums[l + 1]){
-                        l++;
-                    }
+                    triplets.add(Arrays.asList(nums[i], nums[r], nums[l]));
+                    while(l < r && nums[l] == nums[l + 1]) l++;
+                    while(l < r && nums[r] == nums[r - 1]) r--;
                     r--;
                     l++;
-                }else if(sum > 0) {
+                }else if (sum > 0){
                     r--;
                 }else {
                     l++;
                 }
-
             }
-
         }
-
         return triplets;
     }
-
 }
