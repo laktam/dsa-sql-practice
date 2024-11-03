a37837a1743e61a08d4dd22349584c4ebee02fcf
laktam <95582405+laktam@users.noreply.github.com>
2024-11-03 13:10:12 +0100

    Time: 43 ms (5.96%) | Memory: 45.4 MB (20.14%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> charsMap = new LinkedHashMap<>();
+        for(int c = 0; c < s.length(); c++){
+            if(charsMap.containsKey(s.charAt(c))){
+                charsMap.put(s.charAt(c), charsMap.get(s.charAt(c)) + 1);
+            }else{
+                charsMap.put(s.charAt(c), 1);
+            }
+        }
+        for(Map.Entry<Character, Integer> entry : charsMap.entrySet()){
+            if(entry.getValue().equals(1)){
+                return s.indexOf(entry.getKey().toString());
+            }
+        }
+        return -1;
+    }
+}
