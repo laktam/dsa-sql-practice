448d4c9dfac799f608027bf26f4cae10a74a4213
laktam <95582405+laktam@users.noreply.github.com>
2024-11-04 14:50:44 +0100

    Time: 4 ms (20.42%) | Memory: 44.6 MB (64.35%) - LeetSync

--- 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.java
@@ -17,17 +17,37 @@ class Node {
 };
 */
 
+
 class Solution {
     public List<Integer> preorder(Node root) {
         List<Integer> result = new LinkedList<>();
         if(root == null){
             return result;
         }
-        result.add(root.val);
-        for(Node child: root.children){
-            result.addAll(preorder(child));
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while(stack.size() > 0){
+            result.add(root.val);
+            stack.pop();
+            ListIterator it = root.children.listIterator();
+            for(int i = root.children.size() - 1; i >= 0; i--){
+                stack.push(root.children.get(i));
+            }
+            if(stack.isEmpty()) break;
+            root = stack.peek();
         }
-        
         return result;
+
+//  recursive approach
+//         List<Integer> result = new LinkedList<>();
+//         if(root == null){
+//             return result;
+//         }
+//         result.add(root.val);
+//         for(Node child: root.children){
+//             result.addAll(preorder(child));
+//         }
+        
+//         return result;
     }
 }
