91029ebcd98f9fa32b90a5807934d51c9a05862c
laktam <95582405+laktam@users.noreply.github.com>
2024-11-05 18:15:20 +0100

    Time: 8 ms (24.70%) | Memory: 45.4 MB (69.91%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,30 +1,21 @@
 class Solution {
+    private List<String> possibilities = new ArrayList<>();
+
     public List<String> letterCasePermutation(String s) {
-        return letterCasePermutation("", s);
+        letterCasePermutation("", s);
+        return possibilities;
     }
 
-    public List<String> letterCasePermutation(String prefix, String subS) {
-        List<String> possibilities = new ArrayList<>();
+    public void letterCasePermutation(String prefix, String subS) {
         if(subS.length() != 0){
             if(Character.isAlphabetic(subS.charAt(0))){
-                possibilities.addAll(
-                    letterCasePermutation(prefix + Character.toString(Character.toLowerCase(subS.charAt(0))), subS.substring(1))
-                );
-                possibilities.addAll(
-                    letterCasePermutation(prefix +  Character.toString(Character.toUpperCase(subS.charAt(0))), subS.substring(1))
-                );
-                
+                    letterCasePermutation(prefix + Character.toString(Character.toLowerCase(subS.charAt(0))), subS.substring(1));
+                    letterCasePermutation(prefix +  Character.toString(Character.toUpperCase(subS.charAt(0))), subS.substring(1));                
             }else{
-                possibilities.addAll(
-                    letterCasePermutation(prefix + subS.charAt(0), subS.substring(1))
-                );
+                    letterCasePermutation(prefix + subS.charAt(0), subS.substring(1));
             }
         }else{
             possibilities.add(prefix);
-            return possibilities;
         }
-        return possibilities;
-
-
     }
 }
