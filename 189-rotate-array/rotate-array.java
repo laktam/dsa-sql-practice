1a4b95d4995c47abcb6aa9e35fdc42e90b90a25b
laktam <95582405+laktam@users.noreply.github.com>
2024-11-06 10:55:26 +0100

    Time: 0 ms (100.00%) | Memory: 57.5 MB (22.74%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,9 +1,31 @@
 class Solution {
+
+    // in place
     public void rotate(int[] nums, int k) {
-        int help[] = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
-            help[(i + k) % nums.length] = nums[i];
-        }
-        System.arraycopy(help, 0, nums, 0, nums.length);
+        k = k % nums.length;
+        reverse(0, nums.length - 1, nums);
+        reverse(0, k - 1, nums);
+        reverse(k, nums.length - 1, nums);
     }
+
+    private void reverse(int i, int j, int nums[]){
+        while(i < j){
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+
+
+
+    // with extra memory
+    // public void rotate(int[] nums, int k) {
+    //     int help[] = new int[nums.length];
+    //     for(int i = 0; i < nums.length; i++){
+    //         help[(i + k) % nums.length] = nums[i];
+    //     }
+    //     System.arraycopy(help, 0, nums, 0, nums.length);
+    // }
 }
