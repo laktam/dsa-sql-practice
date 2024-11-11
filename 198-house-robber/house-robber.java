89a791d1c3adbb827009de7c22dca11bff3aa400
laktam <95582405+laktam@users.noreply.github.com>
2024-11-11 21:53:41 +0100

    Time: 0 ms (100.00%) | Memory: 40.8 MB (63.74%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,14 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return nums[0] > nums[1] ? nums[0] : nums[1];
-        int dp[] = new int[nums.length];
-        // dp contains the max if we robed that house
-        dp[0] = nums[0];
-        dp[1] = nums[0] > nums[1] ? nums[0] : nums[1];
-        for(int i = 2; i < nums.length; i++){
-            dp[i] = dp[i - 1] > dp[i - 2] + nums[i] ? dp[i - 1] : dp[i - 2] + nums[i];
+        // rob1 : the max up till the i - 2 house [rob1, rob2, i, ....]
+        int rob1 = 0;
+        // rob2 : the max up till the i - 1 house [rob1, rob2, i, ....]
+        int rob2 = 0;
+        for(int i = 0; i < nums.length; i++){
+            // either we rob current house + rob 1 (max robbed till i - 2)
+            // or we take the max robed up to the last house (rob2 )
+
+            // max is the max we can rob from 0 till i
+            // it is either the max robbed from 0 till (i - 2) + nums[i] 
+            // or the max robbed from 0 till (i - 1)
+            int max = Math.max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = max;
         }
-        return dp[nums.length - 1];
+        return rob2;
     }
 }
