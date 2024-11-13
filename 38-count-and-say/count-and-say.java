32e2826630d0cd17aab6cc464b07ff694840b166
laktam <95582405+laktam@users.noreply.github.com>
2024-11-13 22:44:18 +0100

    Time: 18 ms (12.14%) | Memory: 45.5 MB (6.81%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n == 1) return "1";
+        return rLE(countAndSay(n - 1));
+    }
+
+    private String rLE(String s){
+        if(s.length() == 1) {
+            return "1" + s;
+        }
+        String result = "";
+        int count = 1;
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i - 1) == s.charAt(i)) {
+                if(i == s.length() - 1 ){
+                    result += "" + ++count + s.charAt(i);
+                    count = 1;
+                }else{
+                    count++;
+                }
+            }
+            else {
+                if(i == s.length() - 1 ){
+                    result += "" + count + s.charAt(i - 1);
+                    result += "1" + s.charAt(i);
+                }else{
+                    result += "" + count + s.charAt(i - 1);
+                    count = 1;
+                }
+                
+            }
+
+        }
+        return result;
+    }
+}
