d1ce221b63e71360918c442e2b2eb1d8b5874ec6
laktam <95582405+laktam@users.noreply.github.com>
2024-11-18 18:07:53 +0100

    Time: 2253 ms (5.07%) | Memory: 58.3 MB (43.21%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,6 +1,45 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        return quickSelectLargest(nums, k);
+    }
+
+    private int quickSelectLargest(int nums[], int k){
+        int l = 0;
+        int h = nums.length - 1;
+        int target = nums.length - k; // n - k smallest is k largest
+        while(l <= h){
+            int pivot_index = partition(nums, l, h);
+            if(pivot_index == target) return nums[pivot_index];
+            if(pivot_index < target) l = pivot_index + 1;
+            else h = pivot_index - 1;
+        }
+        return -1;
+    }
+
+    private int partition(int nums[], int l , int h){
+        // Randomly select a pivot index
+        int randomIndex = l + (int)(Math.random() * (h - l + 1));
+        // Swap pivot with the last element
+        int t = nums[randomIndex];
+        nums[randomIndex] = nums[h];
+        nums[h] = t;
+
+        int pivot = nums[h];
+        int i = l; // 0 - i : small side
+        for(int j = l; j < h; j++){
+            // this loop move smaller element to the left
+            if(nums[j] < pivot){
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                i++;
+            }
+            
+        }
+        // swap pivot with nums[i]
+        int tmp = nums[i];
+        nums[i] = nums[h];
+        nums[h] = tmp;
+        return i;
     }
 }
