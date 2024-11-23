020ecd19238b44e4adbb224d9ceaf4dedfecaa24
laktam <95582405+laktam@users.noreply.github.com>
2024-11-23 22:00:49 +0100

    Time: 1 ms (66.88%) | Memory: 44.5 MB (78.91%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,15 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // to track unique element
         int unique = 0;
         for(int i = 1; i < nums.length; i++){
-            // found a new unique element
             if(nums[i] != nums[unique]){
-                unique++;//increment to put it next to the unique element before it
+                unique++;
+                int tmp = nums[unique];
                 nums[unique] = nums[i];
+                nums[i] = tmp;
             }
         }
-        return ++unique; 
+        return unique + 1;
     }
 
 }
