63137852bc8bae6c96c0ff224278ef067b5bd69b
laktam <95582405+laktam@users.noreply.github.com>
2024-11-24 15:44:12 +0100

    Time: 0 ms (100.00%) | Memory: 41.6 MB (82.48%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,27 +1,21 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int c = 1;
-
         for(int i = digits.length - 1; i >= 0; i--){
-            int tmp = digits[i] + c;
-            if(tmp > 9){
-                c = tmp - digits[i];
+            if(digits[i] + c > 9){
                 digits[i] = 0;
+                c = 1;
             }else{
                 digits[i] = digits[i] + c;
                 c = 0;
+                break;
             }
         }
-        if(digits.length != 0 && digits[0] == 0){
-            int r[] = new int[digits.length + 1];
-            r[0] = 1;
-            if(digits.length > 1){
-                for(int i = 1; i < digits.length; i++){
-                    r[i + 1] = digits[i];
-                }
-            }
-            
-            return r;
+        if(digits[0] == 0){
+            int out[] = new int[digits.length + 1];
+            System.arraycopy(digits, 0, out, 1, digits.length);
+            out[0] = 1;
+            return out;
         }
         return digits;
     }
