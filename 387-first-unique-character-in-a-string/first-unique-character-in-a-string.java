bfc9d18bce04c99ab9dd1278f936793938fde34a
laktam <95582405+laktam@users.noreply.github.com>
2024-11-25 11:22:14 +0100

    Time: 31 ms (44.67%) | Memory: 44.5 MB (98.97%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -6,12 +6,17 @@ class Solution {
             char c = s.charAt(i);
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int result[] = map.entrySet().stream()
-                .filter(e -> e.getValue() == 1)
-                .mapToInt(e -> s.indexOf(e.getKey()))
-                .toArray();
-        if(result.length < 1) return -1;
-        else return result[0];
+
+        for(Map.Entry<Character, Integer> entry : map.entrySet()){
+            if(entry.getValue() == 1) return s.indexOf(entry.getKey());
+        }
+        return -1;
+        // int result[] = map.entrySet().stream()
+        //         .filter(e -> e.getValue() == 1)
+        //         .mapToInt(e -> s.indexOf(e.getKey()))
+        //         .toArray();
+        // if(result.length < 1) return -1;
+        // else return result[0];
 
         // try{
         //     char unique = set.iterator().next();
