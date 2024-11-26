dab2ccd74cc742d6c0d1808a1009c51aa40d641a
laktam <95582405+laktam@users.noreply.github.com>
2024-11-26 17:09:13 +0100

    Time: 0 ms (100.00%) | Memory: 44.3 MB (52.97%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,18 +11,13 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) return false;
-        ListNode fast = head;
         ListNode slow = head;
-        do{
-            if(fast != null && fast.next != null){
-                fast = fast.next.next;
-                slow = slow.next;
-            }else {
-                return false;
-            }
-            
-        }while(fast != slow);
-        return true;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) return true;
+        }
+        return false;
     }
 }
