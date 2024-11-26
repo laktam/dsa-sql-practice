421e6d33f1e9f7ba2a96bc4457415913e1a71e46
laktam <95582405+laktam@users.noreply.github.com>
2024-11-26 21:27:18 +0100

    Time: 1 ms (90.80%) | Memory: 45 MB (40.30%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,34 +14,22 @@
  * }
  */
 class Solution {
-    private List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if(root == null) return result;
-        List<Integer> l = new ArrayList<>();
-        l.add(root.val);
-        result.add(l);
-        helper(root);
-        return result;
-    }
-
-    public void helper(TreeNode... nodes){
-        List<TreeNode> nextLevel = new ArrayList<>();
-        List<Integer> l = new ArrayList<>();
-        for(TreeNode node : nodes){
-            if(node.left != null){
-                l.add(node.left.val);
-                nextLevel.add(node.left);
+        List<List<Integer>> out = new ArrayList<>();
+        if(root == null) return out;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            List<Integer> l = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                TreeNode n = q.remove();
+                l.add(n.val);
+                if(n.left != null) q.add(n.left);
+                if(n.right != null) q.add(n.right);
             }
-            if(node.right != null){
-                l.add(node.right.val);
-                nextLevel.add(node.right);
-            }
+            out.add(l);
         }
-        if(!l.isEmpty()){
-                result.add(l);
-            }
-        if(!nextLevel.isEmpty()){
-            helper(nextLevel.toArray(new TreeNode[0]));
-        }
+        return out;
     }
 }
