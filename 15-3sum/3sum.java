e2dfe59c2528c1afc22d038a4ae37722aaa81662
laktam <95582405+laktam@users.noreply.github.com>
2024-11-29 22:53:41 +0100

    Time: 31 ms (55.52%) | Memory: 51.5 MB (54.41%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,27 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> triplets = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i < nums.length - 2; i++){
             if(i > 0 && nums[i] == nums[i - 1]) continue;
-            
-            int r = nums.length - 1;
             int l = i + 1;
+            int r = nums.length - 1;
             while(l < r){
-                int sum = nums[i] + nums[r] + nums[l];
-                if(sum == 0){
-                    triplets.add(Arrays.asList(nums[i], nums[r], nums[l]));
-                    while(l < r && nums[l] == nums[l + 1]) l++;
-                    while(l < r && nums[r] == nums[r - 1]) r--;
-                    r--;
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == 0) {
+                    result.add(List.of(nums[i], nums[l], nums[r]));
+                    // go to next unique l and r
+                    while (l < r && nums[l + 1] == nums[l]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
                     l++;
-                }else if (sum > 0){
                     r--;
-                }else {
-                    l++;
                 }
-            }
+                else if (sum > 0) r--;
+                else l++;
+            }    
         }
-        return triplets;
+        return result;
     }
 }
