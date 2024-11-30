509b7da93c182222a39f6b0fa5e7baa0d2966493
laktam <95582405+laktam@users.noreply.github.com>
2024-11-30 20:44:36 +0100

    Time: 3 ms (21.83%) | Memory: 134.1 MB (30.86%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if(nums.length < 3) return false;
-        // will hold the smallest element so far 
-        int first = Integer.MAX_VALUE;
-        // will hold the one just after it
-        int second =  Integer.MAX_VALUE;
+        int small = Integer.MAX_VALUE;
+        int smaller = Integer.MAX_VALUE;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] <= first) first = nums[i];
-            else if(nums[i] <= second) second = nums[i];
-            else return true;// here f < s < nums[i]
+            if(nums[i] < smaller){
+                smaller = nums[i];
+            }else if(smaller < nums[i] && nums[i] < small){
+                small = nums[i];
+            }else if(small < nums[i]){
+                return true;
+            }
         }
         return false;
+        
     }
 }
