7beb4d02a57b101267ac2529635ce5a0c666f652
laktam <95582405+laktam@users.noreply.github.com>
2024-12-06 21:39:34 +0100

    Time: 30 ms (61.22%) | Memory: 54.7 MB (29.60%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Queue<Integer[]> q = new PriorityQueue<Integer[]>((ar1, ar2)->{
+        Queue<int[]> q = new PriorityQueue<int[]>((ar1, ar2)->{
             // calculate distances to 0, 0
-            double dist1 = Math.sqrt(Math.pow(ar1[0], 2) + Math.pow(ar1[1], 2));
-            double dist2 = Math.sqrt(Math.pow(ar2[0], 2) + Math.pow(ar2[1], 2));
+            double dist1 = Math.sqrt(ar1[0] * ar1[0] + ar1[1] * ar1[1]);
+            double dist2 = Math.sqrt(ar2[0] * ar2[0] + ar2[1] * ar2[1]);
             if(dist1 > dist2) return 1;
             if(dist1 < dist2) return -1;
             else return 0;
         });
         for(int i = 0; i < points.length; i++){
-            Integer[] p = new Integer[]{points[i][0], points[i][1]};
-            q.add(p);
+            q.add(points[i]);
         }
         int[][] result = new int[k][2];
         for(int i = 0; i < k; i++){
-            Integer[] e = q.remove();
-            result[i] = new int[] {e[0].intValue(), e[1].intValue()};
+            int[] e = q.remove();
+            result[i] = new int[] {e[0], e[1]};
         }
         return result;
 
