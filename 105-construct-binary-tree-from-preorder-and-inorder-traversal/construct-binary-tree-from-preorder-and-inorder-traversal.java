45c02beddd0ba981eff6e53e258fc4efe0895f21
laktam <95582405+laktam@users.noreply.github.com>
2024-12-06 20:49:54 +0100

    Time: 3 ms (38.05%) | Memory: 44.2 MB (71.37%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,28 +16,27 @@
 class Solution {
     private int n = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return tree(inorder, preorder);
+        return tree(inorder, preorder, 0, inorder.length - 1);
     }
-    private TreeNode tree(int[] inorder, int[] preorder){
-        if(inorder.length == 0 || n >= preorder.length) return null;
+    private TreeNode tree(int[] inorder, int[] preorder, int start, int end){
+        if(n >= preorder.length) return null;
+        if(start == end) return new TreeNode(inorder[start]);
 
         TreeNode root = new TreeNode(preorder[n]);
         int rootIndex = 0;
-        for(int i = 0; i< inorder.length; i++){
+        for(int i = start; i <= end; i++){
             if(inorder[i] == preorder[n]){
                 rootIndex = i;
                 break;
             }
         }
-        if(rootIndex != 0){
-            int[] left = Arrays.copyOfRange(inorder, 0, rootIndex);
+        if(rootIndex > start){
             n++;
-            root.left = tree(left, preorder);
+            root.left = tree(inorder, preorder, start, rootIndex - 1);
         }
-        if(rootIndex + 1 < inorder.length){
-            int[] right = Arrays.copyOfRange(inorder, rootIndex + 1, inorder.length);
+        if(rootIndex < end){
             n++;
-            root.right = tree(right, preorder);
+            root.right = tree(inorder, preorder, rootIndex + 1, end);
         }
         return root;
     }
