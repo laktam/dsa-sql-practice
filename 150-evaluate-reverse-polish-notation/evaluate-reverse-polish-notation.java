1abf8b15287a4a499900327715b690672c27c948
laktam <95582405+laktam@users.noreply.github.com>
2024-12-07 19:26:59 +0100

    Time: 5 ms (97.29%) | Memory: 45 MB (18.46%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,9 +1,8 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < tokens.length; i++){
-            String current = tokens[i];
-            switch (current) {
+        for (String token : tokens) {
+            switch(token){
                 case "-":
                     stack.push(-stack.pop() + stack.pop());
                     break;
@@ -18,28 +17,11 @@ class Solution {
                 case "*":
                     stack.push(stack.pop() * stack.pop());
                     break;
-                default :
-                    stack.push(Integer.parseInt(current));
-            } 
+                default:
+                    stack.push(Integer.parseInt(token));
+                    break;
+            }
         }
         return stack.pop();
-    }
-
-    public boolean isOperation(String s){
-        return s.matches("\\*|\\+|/|-");
-    }
-
-    private int operation(String s1, String s2, String op){
-        int i1 = Integer.parseInt(s1);
-        int i2 = Integer.parseInt(s2);
-        if(op.equals("*")){
-            return i1 * i2;
-        }else if(op.equals("/")){
-            return i1 / i2;
-        }else if(op.equals("-")){
-            return i1 - i2;
-        }else {
-            return i1 + i2;
-        }
     }
 }
