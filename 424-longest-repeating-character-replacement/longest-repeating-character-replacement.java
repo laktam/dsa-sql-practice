4b881f6c00977d112b26149283ded8c0b87942f0
laktam <95582405+laktam@users.noreply.github.com>
2024-12-07 14:35:48 +0100

    Time: 27 ms (29.77%) | Memory: 45.4 MB (7.25%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,21 +1,27 @@
 class Solution {
+    /*
+    keep a freqency map
+    have a window of characters
+    each time test if replacing the other words the most frequent word
+    will give use a repeating substring
+    if not remove first character
+    */
     public int characterReplacement(String s, int k) {
-        int max = 0;
-        int l = 0;
-        int maxFreq = 0;
         Map<Character, Integer> map = new HashMap<>();
-        int length = s.length();
-        for(int r = 0; r < length; r++){
-            char c = s.charAt(r);
+        int start = 0;
+        int maxFreq = 0;
+        int result = 0;
+        for(int end = 0; end < s.length(); end++){
+            char c = s.charAt(end);
             map.put(c, map.getOrDefault(c, 0) + 1);
             maxFreq = Math.max(maxFreq, map.get(c));
-            while((r - l + 1) - maxFreq > k){
-                char toRemoveC = s.charAt(l);
-                map.put(toRemoveC, map.get(toRemoveC) - 1);
-                l++;
+            while((end - start + 1) - maxFreq> k){
+                char startC = s.charAt(start);
+                map.put(startC, map.get(startC) - 1);
+                start++;
             }
-            max = Math.max(max, r - l + 1);
+            result = Math.max(result, end - start + 1);
         }
-        return max;
+        return result;
     }
 }
